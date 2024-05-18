637f78d18ce7444c5424762d33c72d94bafb82dd
HenriqueGomes <henriquegomesjunqueira@gmail.com>
2024-05-18 00:52:05 -0300

    start database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://admin:adminPass@miaudote.rtjemoz.mongodb.net/?retryWrites=true&w=majority&appName=miaudote')],
   controllers: [AppController],
   providers: [AppService],
 })
